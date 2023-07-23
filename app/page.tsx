fa28f917e3ecdbbd4d7ff3b12e181db4db6fcbd9
nawed2611 <nawed2611@gmail.com>
2023-07-23 16:57:35 +0530

    Add Date

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,19 @@ export default function Page() {
   return (
     <div className="min-h-screen flex flex-col justify-evenly items-center sm:px-5">
       <div className="flex w-full justify-between px-12">
-        <h1 className="text-4xl font-inter">MindJournal</h1>
+        <div>
+
+          <h1 className="text-4xl font-inter">MindJournal</h1>
+          <p>{
+            new Date().toLocaleString('en-US', {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+            })
+          }
+          </p>
+        </div>
         <div className="flex gap-x-4">
           <UserButton afterSignOutUrl="/" />
           <Menu />
