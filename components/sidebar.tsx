e963556f077a9ffa038aed95af1bd4e49dae1a3b
Md Anzar Ahmad <anzarhps@gmail.com>
2023-07-23 18:29:17 +0530

    add styling

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,10 +7,10 @@ const Sidebar = ({ data }) => {
     <div className="h-screen p-4">
       <ul>
         {data.map((item) => (
-          <div key={item.id} className="flex">
-            <li>
-              {item.title} <Link href={`/journals/${item.id}`}>View</Link>
-            </li>
+          <div key={item.id} className="my-4 rounded bg-white p-4 shadow-md">
+            <h3 className="text-xl font-semibold">{item.title}</h3>
+            <p className="text-gray-600">{item.content}</p>
+            <Link href={`/journals/${item.id}`}>View</Link>
           </div>
         ))}
       </ul>
